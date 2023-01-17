1d7e9b91f9adf0eb67cc07fc253883f7be4891f2
Bronty <at3177198@gmail.com>
2023-01-17 14:13:54 +0900

    カードにカードのデータを表示

--- main.js
+++ main.js
@@ -1,10 +1,63 @@
-console.log("test")
+console.log(carddata)
 var cards = []
 function GenerateCard(){
     var card = document.createElement('div')
+    var cardnum = Math.floor(Math.random()*120)
+    var newcard = carddata[cardnum]
+    //カードの種類 攻撃、守り、魔法　
+    var cardclass = document.createElement('p')
+    //カードステータス
+    var cardstatus = document.createElement('p')
+    if(newcard.class=="attack"){
+        cardclass.innerText = "攻撃"
+        cardstatus.innerText = newcard.status
+    }else if(newcard.class=="defense"){
+        cardclass.innerText = "守り"
+        cardstatus.innerText = newcard.status
+    }
+    else if(newcard.class=="both"){
+        cardclass.innerText = "攻/守"
+        cardstatus.innerText = newcard.status +"/"+newcard.status2
+    }
+    else if(newcard.class=="HPheal"){
+        cardclass.innerText = "HP回復"
+        cardstatus.innerText = newcard.status
+    }
+    else if(newcard.class=="MPheal"){
+        cardclass.innerText = "MP回復"
+        cardstatus.innerText = newcard.status
+    }else if(newcard.class=="unique"){
+        cardclass.innerText="特殊"
+        cardstatus.innerText = newcard.explanation
+    }
+    //カードのタイプ　無属性、火、水、草
+    var cardtype = document.createElement('p')
+    if(newcard.type=="none"){
+        cardtype.innerText = "無属性"
+    }
+    else if(newcard.type=="fire"){
+        cardtype.innerText = "火"
+    }
+    else if(newcard.type=="water"){
+        cardtype.innerText = "水"
+    }
+    else if(newcard.type=="grass"){
+        cardtype.innerText = "草"
+    }
+    else if(newcard.type=="item"){
+        cardtype.innerText = "アイテム"
+    }
+   
+    //カードの命中率
+    var cardhitrate = document.createElement('p')
+    cardhitrate.innerText ="命中率"+ newcard.hitrate + "%"
+
     card.setAttribute('class','card')
     card.setAttribute('onclick','UseCard(this)')
-    card.innerHTML="<p>"+Math.floor(Math.random()*120)+"</p>"
+    card.appendChild(cardclass)
+    card.appendChild(cardtype)
+    card.appendChild(cardstatus)
+    card.appendChild(cardhitrate)
     document.getElementById('card-group').appendChild(card)
     document.getElementById('card-num').innerText=Math.floor(Math.random()*120)
 }
